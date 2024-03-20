2b44e1483614c4d374baed163052f7dd193a2cc1
hoon0303 <pbd2857@naver.com>
2024-03-20 21:11:31 +0900

    update

--- sort/k번째수.py
+++ sort/k번째수.py
@@ -10,3 +10,8 @@ def solution(array, commands):
         x.sort()
         answer.append(x[k])
     return answer
+
+array = [1, 5, 2, 6, 3, 7, 4]
+commands = [[2, 5, 3], [4, 4, 1], [1, 7, 3]]
+
+print (solution(array,commands))
